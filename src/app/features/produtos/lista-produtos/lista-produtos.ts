31774a3084f4c5acbc2fdaa802d024180f675b10
daniel.paixao <omegadaniel582@gmail.com>
2026-07-17 21:27:38 -0300

    feat: implementa gerenciamento de produtos com signals e effects

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Produto } from '../produto/produto';
+import { Signal } from '@angular/core';
+import { computed } from '@angular/core';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
+import { effect } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto],
+  imports: [Produto,PrecoFormatadoPipe, UpperCasePipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
- produtos = [
+  //!LISTA COM DADOS
+ produtos = signal([
     { 
       nome: 'Teclado Gamer', 
       preco:149.00
@@ -29,8 +35,49 @@ export class ListaProdutos {
       nome: 'Headset Gamer', 
       preco:699.99
     }
-  ];
+  ]);
+  //!Função para exibir produtos selecionados pelo usuario no console
   exibirProduto (nome: string){
     console.log ('Produto Selecionado: ', nome);
+    this.produtoSelecionado.set(nome);
   }
+  //!Função adiciona produto usando metodo update()
+  adicionarProduto(){
+    this.produtos.update(listaAtual => [...listaAtual,
+      {nome:'Playstation 5', preco:3000},
+    ]);
+  }
+  //Função que contabiliza a quantidade  de produtos na lista
+  totalProdutos = computed(() => this.produtos().length);
+
+//!Função calcula o valor total dos produtos usando metodo computed
+valorTotal = computed(()=>
+ {return this.produtos().reduce((total, item) =>
+  total + item.preco,0)});
+  //!Função para substituir a lista atual usando metodo set
+  substituirProdutos(){
+    this.produtos.set([
+      {nome:'Teclado', preco: 50 },
+      {nome:'Mouse', preco: 15 },
+      {nome:'Monitor', preco: 500 },
+      {nome:'Desktop', preco: 1500 },
+      {nome:'Headset', preco: 30 },
+    ]);
+  }
+//!Metodo para monitorar alterações em tempo real usando effect()
+constructor(){
+  effect(() => {
+    console.log('Lista de Produtos Alterados: ', this.produtos());
+  });
+  effect(() => {
+    console.log('Valor Total Atualizado: ', this.valorTotal());
+  });
+  effect(() => {
+    if (typeof document !== 'undefined'){
+      document.title = `(${this.totalProdutos()}) - LoPojapa dopo Ipidanpan`;
+    }
+  });
+}
+//!Metodo para criar um estado de seleção com signal string | null
+produtoSelecionado = signal <string | null>(null); 
 }
